88aecc361e2b4aa43f98963632295f843ce13db0
aashishbiradar <aashish.biradar@gmail.com>
2020-01-06 00:43:39 +0530

    mongodb connected

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
-
 const PORT = process.env.PORT || 3000;
+const connectDB = require('./config/db')
 
 const app = express();
+
+//connect to mongodb
+connectDB();
 
 app.get('/',(req,res) => {
     res.send('api running');
